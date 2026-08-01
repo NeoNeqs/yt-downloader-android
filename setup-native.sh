aace7406fd46417388753740daefabe4bb6536e8
NeoNeqs <76450252+NeoNeqs@users.noreply.github.com>
2026-08-01 22:34:04 +0200

    Minor Fix

--- setup-native.sh
+++ setup-native.sh
@@ -197,7 +197,7 @@ PKG_CONFIG_PATH="$LAME_LIB/pkgconfig"
 
 export PKG_CONFIG_PATH
 
-ls -l "$BUILD_DIR/lame/install/usr/local/lib/libmp3lame.a"
+ls -l "$WORK_DIR/lame/install/usr/local/lib/libmp3lame.a"
 
 ./configure \
     --target-os=android \
